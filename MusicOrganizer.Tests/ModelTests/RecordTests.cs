9c5c1553a3c841a481426fa7f7f96f48f05dbf83
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-30 10:09:57 -0700

    Artist Constructor test taking a property added and passed
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -11,14 +11,20 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void RecordConstructor_CreatesInstanceOfRecord_Record()
     {
-      Record newRecord = new Record("test");
+      Record newRecord = new Record("test", "test");
       Assert.AreEqual(typeof(Record), newRecord.GetType());
     }
     [TestMethod]
     public void RecordConstructor_TakesRecordNameAsAProperty_True()
     {
-      Record testRecord = new Record("album name");
+      Record testRecord = new Record("album name", "test");
       Assert.AreEqual(testRecord.RecordName, "album name");
+    }
+    [TestMethod]
+    public void ArtistConstructor_ReturnsArtistName_Artist()
+    {
+      Record newArtist = new Record("album name", "artist name");
+      Assert.AreEqual(newArtist.ArtistName, "artist name");
     }
   }
 }
